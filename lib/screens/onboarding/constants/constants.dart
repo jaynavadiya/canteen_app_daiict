91fa245a2613ce9b458ab10a229209f093a51544
tri_nity <202001430@daiict.ac.in>
2023-04-07 11:53:14 +0530

    Created a file for storing all constants
    Code reusability increased by introducing constants.dart. Every constants to be added in this file and to be imported wherever the constants are used.

---
+++ lib/screens/onboarding/constants/constants.dart
@@ -0,0 +1,6 @@
+class Constants{
+    static const googleLogoPath = 'assets/icons/google_black.png';
+    static const appleLogoPath = 'assets/icons/apple_black.png';
+    static const emailLogoPath = 'assets/icons/email_black.png';
+    static const passwordLogoPath = 'assets/icons/password_black.png';
+}
