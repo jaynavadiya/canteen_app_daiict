0e59cc1d79595821491cef9a5aa850fafeba5750
jaynavadiya <67496808+jaynavadiya@users.noreply.github.com>
2023-04-08 23:57:38 +0530

    Made a new method customSignInDialog

--- lib/screens/onboarding/onboarding_screen.dart
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -77,96 +77,11 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                   btnAnimationColtroller: _btnAnimationColtroller,
                   press: () {
                     _btnAnimationColtroller.isActive = true;
-                    showGeneralDialog(
-                      barrierDismissible: true,
-                      barrierLabel: "Sign In",
-                      context: context,
-                      pageBuilder: (context, _, __) => Center(
-                        child: Container(
-                          height: 620,
-                          margin: const EdgeInsets.symmetric(horizontal: 16),
-                          padding: EdgeInsets.symmetric(
-                            vertical: 32,
-                            horizontal: 24,
-                          ),
-                          decoration: const BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.all(Radius.circular(40)),
-                          ),
-                          child: Scaffold(
-                            backgroundColor: Colors.transparent,
-                            body: Column(
-                              children: [
-                                const Text(
-                                  "Sign In",
-                                  style: TextStyle(
-                                      fontSize: 34, fontFamily: "Poppins"),
-                                ),
-                                const Padding(
-                                  padding: EdgeInsets.symmetric(vertical: 16),
-                                  child: Text(
-                                    "Order food from your favourite canteen and get it by when you reach the canteen!",
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ),
-                                const SignInForm(),
-                                Row(
-                                  children: const [
-                                    Expanded(child: Divider()),
-                                    Padding(
-                                      padding:
-                                          EdgeInsets.symmetric(horizontal: 16),
-                                      child: Text(
-                                        "OR",
-                                        style: TextStyle(color: Colors.black),
-                                      ),
-                                    ),
-                                    Expanded(child: Divider())
-                                  ],
-                                ),
-                                const Padding(
-                                  padding: EdgeInsets.symmetric(vertical: 20),
-                                  child: Text(
-                                    "Sign up with Email, Apple or Google",
-                                    style: TextStyle(color: Colors.black54),
-                                  ),
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    IconButton(
-                                      padding: EdgeInsets.zero,
-                                      onPressed: () {},
-                                      icon: Image.asset(
-                                        "assets/icons/email_black_2.png",
-                                        height: 64,
-                                        width: 64,
-                                      ),
-                                    ),
-                                    IconButton(
-                                      onPressed: () {},
-                                      icon: Image.asset(
-                                        "assets/icons/apple_black.png",
-                                        height: 64,
-                                        width: 64,
-                                      ),
-                                    ),
-                                    IconButton(
-                                      onPressed: () {},
-                                      icon: Image.asset(
-                                        "assets/icons/google_black.png",
-                                        height: 64,
-                                        width: 64,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
+                    Future.delayed(
+                      Duration(milliseconds: 800),
+                      () {
+                        customSignInDialog(context);
+                      },
                     );
                   },
                 ),
@@ -181,6 +96,116 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
             ),
           ))
         ],
+      ),
+    );
+  }
+
+  Future<Object?> customSignInDialog(BuildContext context) {
+    return showGeneralDialog(
+      barrierDismissible: true,
+      barrierLabel: "Sign In",
+      context: context,
+      pageBuilder: (context, _, __) => Center(
+        child: Container(
+          height: 620,
+          margin: const EdgeInsets.symmetric(horizontal: 16),
+          padding: EdgeInsets.symmetric(
+            vertical: 32,
+            horizontal: 24,
+          ),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(40)),
+          ),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Column(
+                  children: [
+                    const Text(
+                      "Sign In",
+                      style: TextStyle(fontSize: 34, fontFamily: "Poppins"),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      child: Text(
+                        "Order food from your favourite canteen and get it by when you reach the canteen!",
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    const SignInForm(),
+                    Row(
+                      children: const [
+                        Expanded(child: Divider()),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 16),
+                          child: Text(
+                            "OR",
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                        Expanded(child: Divider())
+                      ],
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: Text(
+                        "Sign up with Email, Apple or Google",
+                        style: TextStyle(color: Colors.black54),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: () {},
+                          icon: Image.asset(
+                            "assets/icons/email_black_2.png",
+                            height: 64,
+                            width: 64,
+                          ),
+                        ),
+                        IconButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: () {},
+                          icon: Image.asset(
+                            "assets/icons/apple_black.png",
+                            height: 64,
+                            width: 64,
+                          ),
+                        ),
+                        IconButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: () {},
+                          icon: Image.asset(
+                            "assets/icons/google_black.png",
+                            height: 64,
+                            width: 64,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const Positioned(
+                    bottom: -48,
+                    left: 0,
+                    right: 0,
+                    child: CircleAvatar(
+                      radius: 16,
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        Icons.close,
+                        color: Colors.black,
+                      ),
+                    )),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
