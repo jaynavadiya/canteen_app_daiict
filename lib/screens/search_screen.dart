4cfb14f760b6f7d3a74d41552b891a7c8de8b439
Divya Patel <divyapatel0273@gmail.com>
2023-04-28 02:08:55 +0530

    Search Added

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -10,18 +10,30 @@ class SearchScreen extends StatefulWidget {
   @override
   _SearchScreenState createState() => _SearchScreenState();
 }
-
 class _SearchScreenState extends State<SearchScreen> {
-  Future<QuerySnapshot>? restaurantsDocumentsList;
+  List<QueryDocumentSnapshot<Object?>> restaurantsDocumentsList = [];
   String sellerNameText = "";
 
-initSearchingRestaurant(String textEntered) {
-  restaurantsDocumentsList = FirebaseFirestore.instance
-      .collection("sellers")
-      .where("sellerName", isGreaterThanOrEqualTo: textEntered)
-      .where("sellerName", isLessThan: textEntered.substring(0, textEntered.length - 1) + String.fromCharCode(textEntered.codeUnitAt(textEntered.length - 1) + 1))
-      .get();
-}
+  @override
+  void initState() {
+    super.initState();
+    initSearchingRestaurant("");
+  }
+
+  initSearchingRestaurant(String textEntered) async {
+    QuerySnapshot snapshot =
+        await FirebaseFirestore.instance.collection('sellers').get();
+    List<QueryDocumentSnapshot> filteredDocuments = snapshot.docs.where((doc) {
+      List<String> sellerNameWords =
+          (doc['sellerName'] as String).toLowerCase().split(' ');
+      List<String> queryWords = textEntered.toLowerCase().split(' ');
+      return queryWords.every((queryWord) =>
+          sellerNameWords.any((sellerNameWord) => sellerNameWord.contains(queryWord)));
+    }).toList();
+    setState(() {
+      restaurantsDocumentsList = filteredDocuments;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,43 +73,111 @@ initSearchingRestaurant(String textEntered) {
               ),
             ),
           ),
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-          ),
         ),
-        elevation: 0,
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: FractionalOffset(-2.0, 0.0),
-            end: FractionalOffset(5.0, -1.0),
-            colors: [
-              Color(0xFFFFFFFF),
-              Color(0xFFFAC898),
-            ],
-          ),
-        ),
-        child: FutureBuilder<QuerySnapshot>(
-          future: restaurantsDocumentsList,
-          builder: (context, snapshot) {
-            return snapshot.hasData
-                ? ListView.builder(
-                    itemCount: snapshot.data!.docs.length,
-                    itemBuilder: (context, index) {
-                      Sellers model = Sellers.fromJson(
-                          snapshot.data!.docs[index].data()!
-                              as Map<String, dynamic>);
-                      return SellersDesignWidget(
-                        model: model,
-                        context: context,
-                      );
-                    })
-                : const Center(child: Text("No Record Found"));
-          },
-        ),
+      body: ListView.builder(
+        itemCount: restaurantsDocumentsList.length,
+        itemBuilder: (context, index) {
+          Sellers model = Sellers.fromJson(
+              restaurantsDocumentsList[index].data()! as Map<String, dynamic>);
+          return SellersDesignWidget(
+            model: model,
+            context: context,
+          );
+        },
       ),
     );
   }
 }
+
+
+// class _SearchScreenState extends State<SearchScreen> {
+//   Future<QuerySnapshot>? restaurantsDocumentsList;
+//   String sellerNameText = "";
+
+// initSearchingRestaurant(String textEntered) {
+//   restaurantsDocumentsList = FirebaseFirestore.instance
+//       .collection("sellers")
+//       .where("sellerName", isGreaterThanOrEqualTo: textEntered)
+//       .where("sellerName", isLessThan: textEntered.substring(0, textEntered.length - 1) + String.fromCharCode(textEntered.codeUnitAt(textEntered.length - 1) + 1))
+//       .get();
+// }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         flexibleSpace: Container(
+//           decoration: const BoxDecoration(
+//             gradient: LinearGradient(
+//               begin: FractionalOffset(-2.0, 0.0),
+//               end: FractionalOffset(5.0, -1.0),
+//               colors: [
+//                 Color(0xFFFFFFFF),
+//                 Color(0xFFFAC898),
+//               ],
+//             ),
+//           ),
+//         ),
+//         title: TextField(
+//           onChanged: (textEntered) {
+//             setState(() {
+//               sellerNameText = textEntered;
+//             });
+//             //init search
+//             initSearchingRestaurant(textEntered);
+//           },
+//           decoration: InputDecoration(
+//             hintText: "Search Restaurant here",
+//             hintStyle: const TextStyle(color: Colors.black),
+//             border: InputBorder.none,
+//             suffixIcon: IconButton(
+//               onPressed: () {
+//                 initSearchingRestaurant(sellerNameText);
+//               },
+//               icon: const Icon(
+//                 Icons.search,
+//                 color: Colors.black,
+//               ),
+//             ),
+//           ),
+//           style: const TextStyle(
+//             color: Colors.white,
+//             fontSize: 16,
+//           ),
+//         ),
+//         elevation: 0,
+//       ),
+//       body: Container(
+//         decoration: const BoxDecoration(
+//           gradient: LinearGradient(
+//             begin: FractionalOffset(-2.0, 0.0),
+//             end: FractionalOffset(5.0, -1.0),
+//             colors: [
+//               Color(0xFFFFFFFF),
+//               Color(0xFFFAC898),
+//             ],
+//           ),
+//         ),
+//         child: FutureBuilder<QuerySnapshot>(
+//           future: restaurantsDocumentsList,
+//           builder: (context, snapshot) {
+//             return snapshot.hasData
+//                 ? ListView.builder(
+//                     itemCount: snapshot.data!.docs.length,
+//                     itemBuilder: (context, index) {
+//                       Sellers model = Sellers.fromJson(
+//                           snapshot.data!.docs[index].data()!
+//                               as Map<String, dynamic>);
+//                       return SellersDesignWidget(
+//                         model: model,
+//                         context: context,
+//                       );
+//                     })
+//                 : const Center(child: Text("No Record Found"));
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
