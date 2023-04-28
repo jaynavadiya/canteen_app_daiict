b6f5635e8c5543d5bd22e1f2b99172639e4cfdee
Divya Patel <divyapatel0273@gmail.com>
2023-04-28 10:08:37 +0530

    Fuzzy string matching  search

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:canteen_app_daiict/widgets/design/sellers_design.dart';
-
+import 'package:fuzzywuzzy/fuzzywuzzy.dart';
 import '../models/sellers.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -10,6 +10,7 @@ class SearchScreen extends StatefulWidget {
   @override
   _SearchScreenState createState() => _SearchScreenState();
 }
+
 class _SearchScreenState extends State<SearchScreen> {
   List<QueryDocumentSnapshot<Object?>> restaurantsDocumentsList = [];
   String sellerNameText = "";
@@ -20,18 +21,40 @@ class _SearchScreenState extends State<SearchScreen> {
     initSearchingRestaurant("");
   }
 
-  initSearchingRestaurant(String textEntered) async {
+initSearchingRestaurant(String textEntered) async {
     QuerySnapshot snapshot =
         await FirebaseFirestore.instance.collection('sellers').get();
-    List<QueryDocumentSnapshot> filteredDocuments = snapshot.docs.where((doc) {
-      List<String> sellerNameWords =
-          (doc['sellerName'] as String).toLowerCase().split(' ');
-      List<String> queryWords = textEntered.toLowerCase().split(' ');
-      return queryWords.every((queryWord) =>
-          sellerNameWords.any((sellerNameWord) => sellerNameWord.contains(queryWord)));
-    }).toList();
+    List<Map<String, dynamic>> filteredDocuments = snapshot.docs
+        .map((doc) {
+          int score = tokenSortRatio(textEntered, doc['sellerName']);
+          // print score of each document
+          print(textEntered +
+              " : " +
+              doc['sellerName'] +
+              " : " +
+              score.toString());
+          return {'doc': doc, 'score': score};
+        })
+        .where((element) =>
+            element['score'] != null && (element['score'] as int) > 30)
+        .toList()
+        .cast<Map<String, dynamic>>()
+      ..sort((a, b) => b['score'].compareTo(a['score']));
+    if (filteredDocuments.isEmpty && textEntered.isNotEmpty) {
+      filteredDocuments = snapshot.docs
+          .map((doc) => {
+                'doc': doc,
+                'score': tokenSortRatio(textEntered, doc['sellerName'])
+              })
+          .toList()
+          .cast<Map<String, dynamic>>()
+        ..sort((a, b) => b['score'].compareTo(a['score']))
+        ..take(5);
+    }
     setState(() {
-      restaurantsDocumentsList = filteredDocuments;
+      restaurantsDocumentsList = filteredDocuments
+          .map((e) => e['doc'] as QueryDocumentSnapshot<Object?>)
+          .toList();
     });
   }
 
@@ -89,95 +112,3 @@ class _SearchScreenState extends State<SearchScreen> {
     );
   }
 }
-
-
-// class _SearchScreenState extends State<SearchScreen> {
-//   Future<QuerySnapshot>? restaurantsDocumentsList;
-//   String sellerNameText = "";
-
-// initSearchingRestaurant(String textEntered) {
-//   restaurantsDocumentsList = FirebaseFirestore.instance
-//       .collection("sellers")
-//       .where("sellerName", isGreaterThanOrEqualTo: textEntered)
-//       .where("sellerName", isLessThan: textEntered.substring(0, textEntered.length - 1) + String.fromCharCode(textEntered.codeUnitAt(textEntered.length - 1) + 1))
-//       .get();
-// }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         flexibleSpace: Container(
-//           decoration: const BoxDecoration(
-//             gradient: LinearGradient(
-//               begin: FractionalOffset(-2.0, 0.0),
-//               end: FractionalOffset(5.0, -1.0),
-//               colors: [
-//                 Color(0xFFFFFFFF),
-//                 Color(0xFFFAC898),
-//               ],
-//             ),
-//           ),
-//         ),
-//         title: TextField(
-//           onChanged: (textEntered) {
-//             setState(() {
-//               sellerNameText = textEntered;
-//             });
-//             //init search
-//             initSearchingRestaurant(textEntered);
-//           },
-//           decoration: InputDecoration(
-//             hintText: "Search Restaurant here",
-//             hintStyle: const TextStyle(color: Colors.black),
-//             border: InputBorder.none,
-//             suffixIcon: IconButton(
-//               onPressed: () {
-//                 initSearchingRestaurant(sellerNameText);
-//               },
-//               icon: const Icon(
-//                 Icons.search,
-//                 color: Colors.black,
-//               ),
-//             ),
-//           ),
-//           style: const TextStyle(
-//             color: Colors.white,
-//             fontSize: 16,
-//           ),
-//         ),
-//         elevation: 0,
-//       ),
-//       body: Container(
-//         decoration: const BoxDecoration(
-//           gradient: LinearGradient(
-//             begin: FractionalOffset(-2.0, 0.0),
-//             end: FractionalOffset(5.0, -1.0),
-//             colors: [
-//               Color(0xFFFFFFFF),
-//               Color(0xFFFAC898),
-//             ],
-//           ),
-//         ),
-//         child: FutureBuilder<QuerySnapshot>(
-//           future: restaurantsDocumentsList,
-//           builder: (context, snapshot) {
-//             return snapshot.hasData
-//                 ? ListView.builder(
-//                     itemCount: snapshot.data!.docs.length,
-//                     itemBuilder: (context, index) {
-//                       Sellers model = Sellers.fromJson(
-//                           snapshot.data!.docs[index].data()!
-//                               as Map<String, dynamic>);
-//                       return SellersDesignWidget(
-//                         model: model,
-//                         context: context,
-//                       );
-//                     })
-//                 : const Center(child: Text("No Record Found"));
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
